567f673301d5e9cf76bdcb7f70c9133397f5db4b
Arunpaul Muthupandian <41149078+Arunpaulm@users.noreply.github.com>
2023-05-17 13:03:48 +0100

    Revert "Feature/kanban board"

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -62,44 +62,17 @@
 
     <div class="row">
         @if(count($projects) > 0)
+            <div class="col-12">
+                <h3>Your Projects</h3>
+            </div>
 
-            <section class="content">
-                <div class="card card-solid">
-                    <div class="card-header">
-                        <h3 class="card-title">Your Projects</h3>
+            <div class="col-12 col-md-6">
+                @foreach($projects as $project)
+                    <div class="card">
+                        <a href="{{ route('project.details', $project->slug) }}"><div class="card-header text-center">{{ $project->name }}</div></a>
                     </div>
-                    <div class="card-body pb-0">
-                        <div class="row">
-                            @foreach($projects as $project)
-                                <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch flex-column">
-                                    <div class="card bg-light d-flex flex-fill">
-                                        <a href="{{ route('project.details', $project->slug) }}">
-                                            <div class="card-header text-muted border-bottom-0">
-                                            </div>
-                                            <div class="card-body pt-0">
-                                                <div class="row">
-                                                    <div class="col-7">
-                                                        <h2 class="lead"><b>{{ $project->name }}</b></h2>
-                                                        <p class="text-muted text-sm"><b>Description: </b> {{ \Illuminate\Support\Str::limit($project->description, 100, $end=' ...') }} </p>
-                                                        <ul class="ml-4 mb-0 fa-ul text-muted">
-                                                            <li class="small"><span class="fa-li"><i class="fas fa-lg fa-calendar"></i></span> Start Date: {{ \Carbon\Carbon::parse($project->start_date)->format('d-M-Y') }}</li>
-                                                            <li class="small"><span class="fa-li"><i class="fas fa-lg fa-calendar"></i></span> End Date: {{ \Carbon\Carbon::parse($project->end_date)->format('d-M-Y') }}</li>
-                                                        </ul>
-                                                    </div>
-                                                    <div class="col-5 text-center">
-                                                        <img src="{{ \Illuminate\Support\Facades\Storage::disk('public')->url($project->avatar_image) }}" alt="user-avatar" class="img-circle img-fluid">
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </a>
-                                    </div>
-                                </div>
-                            @endforeach
-                        </div>
-                    </div>
-                </div>
-            </section>
-
+                @endforeach
+            </div>
         @endif
     </div>
 </div>
